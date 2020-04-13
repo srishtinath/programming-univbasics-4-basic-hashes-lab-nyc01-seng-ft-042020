564c4a9184772aa28a83048e69f2ad6fc30db518
Srishti Nath <srishti.nath@gmail.com>
2020-04-13 02:30:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    favorite_color: "yellow",
+    favorite_breakfast: "eggs",
+    favorite_season: "fall"
+  }
 end
 
 def pioneer
